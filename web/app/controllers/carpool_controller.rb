c230a57bffcee2a060f57aba40fd740ff2c2c316
Gonzalo Moya <gonzalo@localhost.localdomain>
2014-07-25 13:53:45 -0400

    algo en controller de carpool

--- web/app/controllers/carpool_controller.rb
+++ web/app/controllers/carpool_controller.rb
@@ -1,6 +1,15 @@
 class CarpoolController < ApplicationController
+	
+	# definir parametros
+	# definir las cosas previas
+	# definir los requerimientos para los 
+
 	def publicar
+		@publicacioncarpool = PublicacionCarpool.create(parametros)
+		# si se falta mucho pero fui almorzar xD
 	end
+
+
 
 	def detalle
 	end
