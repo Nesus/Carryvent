74fa294c05d5abf8db1cb6884b48c34f59b012a6
Camilo Valenzuela <cacke91@gmail.com>
2014-07-27 16:16:55 -0400

    No habia dejado de seguir los archivos en el commit anterior, ahora si estan borrados

--- web/app/controllers/user_controller.rb
+++ web/app/controllers/user_controller.rb
@@ -1,7 +1,7 @@
 class UserController < ApplicationController
 
-
-	before_filter :authenticate_user!, :except => [:index]  
+	before_filter :authenticate_user!, :except => [:index]
+	  
 	def index
 	end
 
