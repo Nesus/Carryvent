4e2a9e887d1567428b47a8276f67aa1337713b7d
Camilo Valenzuela <cacke91@gmail.com>
2014-09-02 02:44:10 -0400

    Arreglada ruta cuando esta logeado

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -90,7 +90,11 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'user#index'
+  authenticated :user do
+    root :to => "evento#eventos", as: "authenticated_root"
+  end
+
+  root :to => 'user#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
