15b55568c6c9eda8d59850086fa42be85c49e703
Cristopher Barraza <cristopher_barraza@hotmail.com>
2014-09-01 22:22:16 -0400

    Formulario para agregar organizaciones y se agrego el campo regiones al formulario de eventos

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -13,23 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140902001813) do
 
-  create_table "activities", force: true do |t|
-    t.integer  "trackable_id"
-    t.string   "trackable_type"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.string   "key"
-    t.text     "parameters"
-    t.integer  "recipient_id"
-    t.string   "recipient_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
-
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -79,16 +62,6 @@ ActiveRecord::Schema.define(version: 20140902001813) do
   end
 
   add_index "eventos", ["publicador_id"], name: "index_eventos_on_publicador_id"
-
-  create_table "gustos", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "gustos", ["category_id"], name: "index_gustos_on_category_id"
-  add_index "gustos", ["user_id"], name: "index_gustos_on_user_id"
 
   create_table "oferta", force: true do |t|
     t.string   "nombre"
@@ -172,18 +145,6 @@ ActiveRecord::Schema.define(version: 20140902001813) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "transaccion_carpools", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "publicacion_carpool_id"
-    t.boolean  "aceptado"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "asientos"
-  end
-
-  add_index "transaccion_carpools", ["publicacion_carpool_id"], name: "index_transaccion_carpools_on_publicacion_carpool_id"
-  add_index "transaccion_carpools", ["user_id"], name: "index_transaccion_carpools_on_user_id"
 
   create_table "user_eventos", force: true do |t|
     t.integer  "user_id"
