535f7aaeabf70d614f512900558d271a33e67bbe
Gonzalo Esteban Moya Rodriguez <gemoya@inf.utfsm.cl>
2014-09-01 22:33:56 -0400

    vista notificaciones pero sin la linea de ellas

--- web/app/controllers/notifications_controller.rb
+++ web/app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
 	def index
-		@activities = PublicActivity::Activity.all
+		@activities = PublicActivity::Activity.where(:owner_id => current_user.id)
 		#@activities = PublicActivity::Activity.all
 	end
 end
