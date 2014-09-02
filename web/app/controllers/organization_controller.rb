9ac3d47ddb04e61c92a7cb3c4d8a5d1e2da6100a
Camilo Valenzuela <cacke91@gmail.com>
2014-09-02 14:41:16 -0400

    Agregado algunos metodos necesarios

--- web/app/controllers/organization_controller.rb
+++ web/app/controllers/organization_controller.rb
@@ -4,12 +4,32 @@ class OrganizationController < ApplicationController
 	end
 
 	def create
+		organization = Organization.new(organization_params)
+		if organization.save
+			redirect_to mostrar_organization_path(organization)
+		else
+			#AGREGAR ERROR
+			redirect_to publicar_organization_path
+		end
 	end
 
 	def show
+		@organization = Organization.find(params[:id])
+	end
+
+	def edit
+		@organization = Organization.find(params[:id])
 	end
 
 	def update
+		organization = Organization.find(params[:id])
+		organization.update(organization_params)
+	end
+
+	private
+
+	def organization_params
+		params.require(:organization).permit(:name, :contact_person, :phone, :facebook, :twitter, :picture)
 	end
 
 end
