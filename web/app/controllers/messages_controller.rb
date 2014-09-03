6881ad18420be2bfafdde88ce57d811abe9632b0
Gonzalo Esteban Moya Rodriguez <gemoya@inf.utfsm.cl>
2014-09-02 23:35:02 -0400

    gg mailboxer

---
+++ web/app/controllers/messages_controller.rb
@@ -0,0 +1,18 @@
+class MessagesController < ApplicationController
+	
+	def index
+	end
+	# GET /message/new
+	def new
+    	@user = User.find(params[:user])
+    	@message = current_user.messages.new
+  	end
+ 
+   	# POST /message/create
+  	def create
+    	@recipient = User.find(params[:user])
+    	current_user.send_message(@recipient, params[:body], params[:subject])
+    	flash[:notice] = "Message has been sent!"
+    	redirect_to :conversations
+  	end
+end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -98,6 +98,24 @@ Rails.application.routes.draw do
     root :to => "evento#eventos_publicador", as: "authenticated_publicador_root"
   end
 
+  resources :messages do
+    member do
+      post :new
+    end
+  end
+  
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+    collection do
+      get :trashbin
+      post :empty_trash
+    end
+  end
+
   root :to => 'user#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
