ea2b96e207a7ab9bee7f9603063de29478b128a7
Gonzalo Moya <gemoya@inf.utfsm.cl>
2014-09-13 21:23:49 -0300

    asd random

--- web/app/models/pasaje.rb
+++ web/app/models/pasaje.rb
@@ -1,6 +1,9 @@
 class Pasaje < ActiveRecord::Base
 	include PublicActivity::Common
-  belongs_to :user_evento
-  belongs_to :oferta
-  belongs_to :bus
+  	
+  	image_accessor :qr_code
+  	
+  	belongs_to :user_evento
+  	belongs_to :oferta
+  	belongs_to :bus
 end
