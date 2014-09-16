56fe915bd7c88704adc57a3db238fc79c484e6f8
Camilo Valenzuela <cacke91@gmail.com>
2014-09-16 15:56:18 -0300

    Cambiado domain en development.rb

--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com",
+    :domain               => "carryvent.com",
     :user_name            => "carryvent@gmail.com",
     :password             => "hola somos carryvent!",
     :authentication       => 'plain',
