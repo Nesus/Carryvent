00be024a18ec4be8b6f291d4930c60001f4190b0
Gortiz08 <ortiz.o.g@gmail.com>
2014-10-09 02:24:42 -0300

    email con tablita en comprar

--- web/app/mailers/venta_mailer.rb
+++ web/app/mailers/venta_mailer.rb
@@ -5,6 +5,8 @@ class VentaMailer < ActionMailer::Base
   	@reserva = reserva	
   	@user = reserva.user_evento.user
   	@evento = reserva.user_evento.evento
+  	attachments.inline['photo.png'] = File.read('app/assets/images/logoCarryvent.png')
+  	attachments.inline['photo1.png'] = File.read('app/assets/images/minilogo.png')
     mail(to: @user.email, subject: 'Compra de su(s) Pasaje(s)')
   end
 end
