3560d275df1934b9aabbc279be8ae4d66ea481cd
Camilo Valenzuela <cacke91@gmail.com>
2014-10-22 23:36:46 -0300

    Agregado date y time picker

--- web/app/assets/javascripts/application.js
+++ web/app/assets/javascripts/application.js
@@ -17,6 +17,9 @@
 //= require jquery.backstretch
 //= require isotope.pkgd
 //= require fittext
+//= require imagesloaded
+//= require jquery.sticky-kit.min
+//= require jquery.timepicker
 
 //= require bootstrap
 //= require underscore
@@ -27,3 +30,22 @@
 
 
 //= require_tree .
+
+ $.datepicker.regional['es'] = {
+ closeText: 'Cerrar',
+ prevText: '<Ant',
+ nextText: 'Sig>',
+ currentText: 'Hoy',
+ monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+ monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+ dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+ dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+ dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+ weekHeader: 'Sm',
+ dateFormat: 'dd/mm/yy',
+ firstDay: 1,
+ isRTL: false,
+ showMonthAfterYear: false,
+ yearSuffix: ''
+ };
+ $.datepicker.setDefaults($.datepicker.regional['es']);
