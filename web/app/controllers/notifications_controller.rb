09537547560fb430f6b0020a7f0260c2513658bb
Camilo Valenzuela <cacke91@gmail.com>
2014-10-26 01:30:45 -0300

    Arreglada un poco la vista de notificaciones

--- web/app/controllers/notifications_controller.rb
+++ web/app/controllers/notifications_controller.rb
@@ -1,9 +1,13 @@
 class NotificationsController < ApplicationController
+	include ActionController::Live
+
 	def index
+		last_checked = current_user.last_checked
 		current_user.update(:last_checked => DateTime.now)
 		last_notifications
 		if current_user
-			@activities = PublicActivity::Activity.where(:recipient_id => current_user.id, :recipient_type => 'User').order("created_at desc").limit(10)
+			@activitiesOld = PublicActivity::Activity.where(:recipient_id => current_user.id, :recipient_type => 'User').order("created_at desc")
+			@activities = PublicActivity::Activity.where(:recipient_id => current_user.id, :recipient_type => 'User').where("created_at > ?", last_checked)
 		elsif current_publicador
 			@activities = PublicActivity::Activity.where(:recipient_id => current_publicador.id, :recipient_type => 'Publicador').order("created_at desc").limit(10)
 		end
